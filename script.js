551fbfe7c2dd928ddf4f3225a713080b2d703c81
Rafidainmj <altrkymjtby9@gmail.com>
2026-07-07 01:28:06 +0300

    Update script.js

--- script.js
+++ script.js
@@ -9,3 +9,15 @@ const title = document.getElementById("title");
 restaurantName.addEventListener("input", () => {
     title.textContent = restaurantName.value || "اسم المطعم";
 });});
+const imageInput = document.getElementById("imageInput");
+const previewImage = document.getElementById("previewImage");
+
+imageInput.addEventListener("change", () => {
+    const file = imageInput.files[0];
+
+    if (file) {
+        previewImage.src = URL.createObjectURL(file);
+        previewImage.style.display = "block";
+    }
+});
+}); 
